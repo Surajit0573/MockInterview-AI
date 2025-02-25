83a47e620506a2ba9f7aae80ed6443bbf2fb601e
Saikat123Dev <berasaikat729@gmail.com>
2025-02-25 23:39:15 +0530

    add email notification API and fetch interviewers endpoint; update mock interview fetching logic

--- src/app/api/mockInterview/mockInterviewList/route.ts
+++ src/app/api/mockInterview/mockInterviewList/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request:NextRequest){
     const user=await currentUser();
+    console.log("user:",user);
     const userid=user?.id;
     try{
         const fetchAllInterviews=await db.mockInterview.findMany({
@@ -13,7 +14,7 @@ export async function GET(request:NextRequest){
             }
         })
         return NextResponse.json(fetchAllInterviews);
-       
+
 
     }catch(error:any){
         console.log("error in fetching all Interviews",error.message||error);
@@ -21,7 +22,7 @@ export async function GET(request:NextRequest){
             {message:"error fetching all interview questions"},
             {status:500}
         );
-        
+
     }
 
 }
